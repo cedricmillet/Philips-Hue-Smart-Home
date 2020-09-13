c66454f62f4b1bcb69a6c3c03426519ecc047d63
Cédric MILLET <milletcedric83400@gmail.com>
2020-09-13 14:21:12 +0200

    adds manipulateLights example

---
+++ examples/manipulateLights.dart
@@ -0,0 +1,39 @@
+import 'package:philips_hue_dart/Hue/Hue.dart';
+import 'package:philips_hue_dart/Hue/Light.dart';
+
+/**
+ * This example show how to manipulate Philips Hue Lights
+ */
+
+
+void main() => demo();
+
+Future<void> demo() async {
+  //  Get connected bridge on local network
+  Hue bridge = await Hue.getBridge();
+  if(bridge==null) throw 'No Philips Hue Bridge on local network.';
+
+  //  Test credentials
+  bridge.username = 'wduRf8BKAppZ2noNbnpkp4r4pED0sas22A-1-UMV';
+
+  //  Get all lights 
+  List<Light> lights = await bridge.getLights() as List<Light>;
+  if(lights.length == 0) throw 'No light detected.';
+
+  //  Display lights data
+  for (Light l in lights) {
+    print(">>>>>>>>>>>>");
+    print(l);
+    print("<<<<<<<<<<<<");
+  }
+
+  //  Switch OFF all lights
+  for (Light l in lights)
+    l.off();
+  
+  //  Manipulate specific light
+  Light m = lights[0] as Light;
+  print('STATE: ${m.get_on()}');
+
+}
+
