f20eff4ec51779b6f14be461104e9252a5594caa
Cédric MILLET <milletcedric83400@gmail.com>
2020-09-16 19:23:53 +0200

    adds batch local deploy script

---
+++ deploy.bat
@@ -0,0 +1,39 @@
+:: Ce script permet de copier le package (contenu du dossier lib) dans le dossier selectionné
+@echo off
+title SCRIPT DEPLOIEMENT PACKAGE
+
+
+:: SOURCE PATH
+set CURRENT_PATH=%cd%
+set SOURCE_PACKAGE_PATH=%CURRENT_PATH%\lib\Hue
+
+:: TARGET PATH
+set DEPLOY_PATH=C:\Users\mille\Desktop\DEV\morphy\lib
+set TARGET_PACKAGE_PATH=%DEPLOY_PATH%\Hue
+
+:: USER CONFIRMATION
+echo.
+echo /!\ Verifiez les chemins avant de poursuivre /!\
+echo.
+echo SOURCE_PACKAGE_PATH             %SOURCE_PACKAGE_PATH% (!! ce dossier sera supprime !!)
+echo TARGET_PACKAGE_PATH             %TARGET_PACKAGE_PATH%
+echo.
+echo.
+PAUSE
+
+::  DELETE
+echo ---- Suppression ancien package....
+rmdir %TARGET_PACKAGE_PATH% /s /q
+
+:: COPY
+echo ---- Copie nouveau package....
+robocopy %SOURCE_PACKAGE_PATH% %TARGET_PACKAGE_PATH% /s /e
+
+
+echo.
+echo.
+echo COPIE TERMINEE.
+echo.
+echo.
+PAUSE
+EXIT
