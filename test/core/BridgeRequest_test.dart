6d6940408f75e36039a2a0b0bb914a0069f66295
cmillet601 <milletcedric83400@gmail.com>
2020-09-22 00:07:27 +0200

    adds unit tests

---
+++ test/core/BridgeRequest_test.dart
@@ -0,0 +1,10 @@
+import 'package:philips_hue_dart/Hue/core/BridgeRequest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('get() method must returns a response as string', () async {
+    final String res = await new BridgeRequest().get('http://www.github.com');
+
+    expect(res.length > 0, true);
+  });
+}
